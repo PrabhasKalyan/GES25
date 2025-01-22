fa1e33006401321ea0ea30cf4af5313ab639fbff
PrabhasKalyan <prabhaskalyan0473@gmail.com>
2025-01-22 13:17:55 +0530

    before photos_final

--- src/@shadcn/ui/components/ui/globe.jsx
+++ src/@shadcn/ui/components/ui/globe.jsx
@@ -175,7 +175,7 @@ export function World(props) {
   const scene = new Scene();
   scene.fog = new Fog(0xffffff, 400, 2000);
   return (
-    (<Canvas style={{ width: '100%', height: '50vh' }} scene={scene} camera={new PerspectiveCamera(50, aspect, 180, 1800)}>
+    (<Canvas style={{ width: '50vh', height: '50vh' }} scene={scene} camera={new PerspectiveCamera(50, aspect, 180, 1800)}>
       <WebGLRendererConfig />
       <ambientLight color={globeConfig.ambientLight} intensity={0.6} />
       <directionalLight
